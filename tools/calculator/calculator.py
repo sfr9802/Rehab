1428d2f2a1632be4905d5d5a55a6f16d569b3620
arin <sfr9802@daum.net>
2025-12-28 17:34:56 +0900

    opp test

---
+++ tools/calculator/calculator.py
@@ -0,0 +1,11 @@
+from types import List
+
+class Calculator :
+    
+    def __init__(self, *args, **kwargs) :
+        self.args = args
+        self.kwargs = kwargs
+    
+    def add(self:List) -> float :
+        input_list = in
+    
---
+++ tools/utils.py
@@ -0,0 +1,5 @@
+from types import List
+
+#입력 파라미터 > 리스트로 파싱 
+def input_parser(self : str) -> List:
+    return self.args.split(" ")
